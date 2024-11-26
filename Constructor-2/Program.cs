4cb970b11560b7d1642c0a855dcd874ebb0526fe
troubleman96 <itslugenge96@gmail.com>
2024-11-26 10:26:34 +0300

    unoo

---
+++ Constructor-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Car
+    {
+         string name;
+         int price;
+        
+        Car(string theName, int thePrice)
+        {
+            theName = name;
+            thePrice = price;
+        }
+
+        static void Main(string[] arg)
+        {
+            Car car1 = new Car("Fortuna", 120000);
+
+            Console.WriteLine($"the name of the car is {car1.name},and the price is {car1.price}");
+        }
+    }
+}
+
+
